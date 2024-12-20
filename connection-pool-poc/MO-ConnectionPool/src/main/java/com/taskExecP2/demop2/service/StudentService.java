b11553823390c2ae9871dcbf516dc19734417321
shivam <76024545+dextroshivam@users.noreply.github.com>
2024-12-20 12:13:33 +0530

    Add files via upload

---
+++ connection-pool-poc/MO-ConnectionPool/src/main/java/com/taskExecP2/demop2/service/StudentService.java
@@ -0,0 +1,28 @@
+package com.taskExecP2.demop2.service;
+
+import com.taskExecP2.demop2.model.Student;
+import com.taskExecP2.demop2.repository.StudentRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Transactional
+    public void saveAllStudents(List<Student> students) {
+        int count=0;
+        for (Student student : students) {
+            System.out.println("Saved student: " + student.getName()+" count "+count++);
+            studentRepository.save(student);
+        }
+    }
+
+//    public void saveAllStudents(List<Student> students) {
+//        studentRepository.saveAll(students);
+//    }
+}
---
+++ connection-pool-poc/MO-ConnectionPool/src/test/java/com/taskExecP2/demop2/Demop2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskExecP2.demop2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demop2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
